2215b6a7d70ff4783ca16dacaccbed0f281ef7f5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-03-29 20:27:33 +0100

    Initial commit

---
+++ src/main/java/com/vijaygenius123/usermgmtspringnext/UserMgmtSpringNextApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.usermgmtspringnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMgmtSpringNextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMgmtSpringNextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/usermgmtspringnext/UserMgmtSpringNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.usermgmtspringnext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMgmtSpringNextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
